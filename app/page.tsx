b563b585bf1ceadb3b95fd3ce105e9d1b6cd8af8
Marquis Sampson <marquis4484@gmail.com>
2025-04-18 14:06:17 -0400

    buttun update

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 import { Carousel } from "@/components/ui/carousel";
 import Image from "next/image";
 import Link from "next/link";
-import Darkmode from "../public/images/buttons/moon_button.png";
+import Darkmode from "../public/images/buttons/Subtract.png";
 import Comic from "../public/images/comic/comic_end.png";
 import LinkedIn from "../public/images/buttons/linkedIn_button.png";
 import Coffee from "../public/images/buttons/BMAC_button.png";
@@ -16,11 +16,9 @@ function Home() {
           id="button"
           className="h-[140px] w-[185px] flex items-end justify-end"
         >
-          <Image
-            src={Darkmode}
-            alt="Darkmode"
-            className="w-[4rem] hover:grayscale "
-          />
+          <button className="bg-[#E3E3E3] p-3 rounded-full duration-200 hover:bg-[#cecece] shadow-[3px_5px_0px_#4f4f4f]">
+            <Image src={Darkmode} alt="Darkmode" className="  h-10 w-10 " />
+          </button>
         </div>
         <div className="flex flex-col justify-center items-center h-[80vh]">
           <div className="w-64 h-16 bg-primary rounded-[20px] border-4 border-black drop-shadow-lg text-center mb-12">
@@ -32,9 +30,14 @@ function Home() {
         </div>
       </div>
 
-      <div id="social_links" className="flex">
-        <Image src={LinkedIn} alt="Comic" className="w-[4rem] " />
-        <Image src={Coffee} alt="Comic" className="w-[4rem] " />
+      <div id="social_links" className="flex justify-evenly">
+        <button className="bg-[#D5BBA1] py-3 px-4 rounded-full duration-200 hover:bg-[#b7a08a] shadow-[3px_5px_0px_#4f4f4f] ">
+          <Image src={Coffee} alt="Darkmode" className="  h-10 w-8" /> 
+          {/* #a38f7b */}
+        </button>
+        <button className="bg-[#D5BBA1] p-3 rounded-full duration-200 hover:bg-[#b7a08a] shadow-[3px_5px_0px_#4f4f4f]">
+          <Image src={LinkedIn} alt="Darkmode" className="h-10 w-10" />
+        </button>
       </div>
     </div>
   );
