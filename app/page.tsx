e16ac40addbf6337481ecae2b636b81d00759ad0
Marquis Sampson <marquis4484@gmail.com>
2025-04-30 14:05:46 -0400

    change

--- app/page.tsx
+++ app/page.tsx
@@ -16,12 +16,12 @@ function Home() {
           id="button"
           className="h-[140px] w-[185px] flex items-end justify-end"
         >
-          <button className="bg-[#E3E3E3] p-3 rounded-full duration-200 hover:bg-[#cecece] shadow-[3px_5px_0px_#4f4f4f]">
+          <button className="bg-[#E3E3E3] p-3 inset-shadow-xs rounded-full duration-200 hover:bg-[#cecece] active:bg-[#9a9a9a]  shadow-[3px_5px_0px_#4f4f4f]  active:shadow-[0px_0px_0px_#4f4f4f] active:duration-100">
             <Image src={Darkmode} alt="Darkmode" className="  h-10 w-10 " />
           </button>
         </div>
         <div className="flex flex-col justify-center items-center h-[80vh]">
-          <div className="w-64 h-16 bg-primary rounded-[20px] border-4 border-black drop-shadow-lg text-center mb-12">
+          <div className="w-64 h-16 bg-primary rounded-[20px] border-4 border-black drop-shadow-lg text-center mb-12 ">
             Comic Title
           </div>
           <Link href="/comic">
@@ -32,7 +32,7 @@ function Home() {
 
       <div id="social_links" className="flex justify-evenly">
         <button className="bg-[#D5BBA1] py-3 px-4 rounded-full duration-200 hover:bg-[#b7a08a] shadow-[3px_5px_0px_#4f4f4f] ">
-          <Image src={Coffee} alt="Darkmode" className="  h-10 w-8" /> 
+          <Image src={Coffee} alt="Darkmode" className="  h-10 w-8" />
           {/* #a38f7b */}
         </button>
         <button className="bg-[#D5BBA1] p-3 rounded-full duration-200 hover:bg-[#b7a08a] shadow-[3px_5px_0px_#4f4f4f]">
