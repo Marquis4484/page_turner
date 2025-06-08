0b534329f806d2df7dbf2f816b206b076000e154
Marquis Sampson <marquis4484@gmail.com>
2025-06-08 14:57:28 -0400

    button update

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,7 @@
+"use client";
 import React from "react";
+import { useState } from "react";
+
 import Image from "next/image";
 import Link from "next/link";
 import Darkmode from "../public/images/buttons/Subtract.png";
@@ -7,6 +10,12 @@ import LinkedIn from "../public/images/buttons/linkedIn_button.png";
 import Coffee from "../public/images/buttons/BMAC_button.png";
 
 function Home() {
+  const [isJiggling, setIsJiggling] = useState(false);
+
+  const handleClick = () => {
+    setIsJiggling(true);
+    setTimeout(() => setIsJiggling(false), 300); // match animation duration
+  };
   return (
     <div className="bg-bannerImg bg-repeat bg-bottom w-full h-screen">
       <div id="comic_carousel_darkmode " className="h-[80vh]">
@@ -14,7 +23,11 @@ function Home() {
           id="button"
           className="h-[140px] w-[185px] flex items-end justify-end"
         >
-          <button className="bg-[#E3E3E3] p-3 inset-shadow-xs rounded-full duration-200 hover:bg-[#cecece] active:bg-[#9a9a9a]  shadow-[3px_5px_0px_#4f4f4f]  active:shadow-[0px_0px_0px_#4f4f4f] active:duration-100">
+          <button
+            className={`bg-[#E3E3E3] p-3 inset-shadow-xs rounded-full duration-200  hover:bg-[#cecece] active:bg-[#9a9a9a] shadow-[3px_5px_0px_#4f4f4f]  active:shadow-[0px_0px_0px_#4f4f4f] active:duration-100  transition-transform  active:translate-y-[2px] active:translate-x-[2px] ${
+              isJiggling ? "animate-jiggle" : ""
+            }`}
+          >
             <Image src={Darkmode} alt="Darkmode" className="  h-10 w-10 " />
           </button>
         </div>
@@ -29,7 +42,12 @@ function Home() {
       </div>
 
       <div id="social_links" className="flex justify-evenly">
-        <button className="bg-[#D5BBA1] py-3 px-4 rounded-full duration-200 hover:bg-[#b7a08a] shadow-[3px_5px_0px_#4f4f4f]  active:shadow-[0px_0px_0px_#4f4f4f] active:duration-100">
+        <button
+          onClick={handleClick}
+          className={`bg-[#D5BBA1] py-3 px-4 rounded-full duration-200 hover:bg-[#b7a08a] shadow-[3px_5px_0px_#4f4f4f]  active:shadow-[0px_0px_0px_#4f4f4f] active:duration-100  transition-transform  active:translate-y-[2px] active:translate-x-[2px] ${
+            isJiggling ? "animate-jiggle" : ""
+          }`}
+        >
           <Image src={Coffee} alt="Darkmode" className="  h-10 w-8" />
           {/* #a38f7b */}
         </button>
