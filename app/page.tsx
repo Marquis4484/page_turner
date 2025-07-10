c7e10525e9878b620d6078d80b9ae87d2a0dfff4
Marquis Sampson <marquis4484@gmail.com>
2025-07-10 14:25:55 -0400

    id update

--- app/page.tsx
+++ app/page.tsx
@@ -10,21 +10,14 @@ import LinkedIn from "../public/images/buttons/linkedIn_button.png";
 import Coffee from "../public/images/buttons/BMAC_button.png";
 
 function Home() {
-  const [isJiggling, setIsJiggling] = useState(false);
 
-  const handleClick = () => {
-    setIsJiggling(true);
-  };
 
   return (
-    <div className="bg-bannerImg bg-repeat bg-bottom w-full h-screen">
-      <div id="comic_carousel_darkmode " className="h-[80vh]">
-        <div
-          id="button"
-          className="h-[140px] w-[185px] flex items-end justify-end"
-        >
-          <button className="bg-[#E3E3E3] p-3 inset-shadow-xs rounded-full duration-200  hover:bg-[#cecece] active:bg-[#9a9a9a] shadow-[3px_5px_0px_#4f4f4f]  active:shadow-[0px_0px_0px_#4f4f4f] active:duration-100  active:translate-y-[2px] active:translate-x-[2px]">
-            <Image src={Darkmode} alt="Darkmode" className="  h-10 w-10 " />
+    <div id="frontpage" className="bg-bannerImg">
+      <div id="elements_frontpage" >
+        <div id="button_position" className="h-[140px] w-[185px] flex items-end justify-end" >
+          <button id="darkmode_button"className="bg-[#E3E3E3] p-3 inset-shadow-xs rounded-full duration-200  hover:bg-[#cecece] active:bg-[#9a9a9a] shadow-[3px_5px_0px_#4f4f4f]  active:shadow-[0px_0px_0px_#4f4f4f] active:duration-100  active:translate-y-[2px] active:translate-x-[2px]">
+            <Image src={Darkmode} alt="Darkmode" className="h-10 w-10"/>
           </button>
         </div>
         <div className="flex flex-col justify-center items-center h-[80vh]">
@@ -39,7 +32,7 @@ function Home() {
 
       <div id="social_links" className="flex justify-evenly">
         <button
-          onClick={handleClick}
+          
           className="bg-[#D5BBA1] py-3 px-4 rounded-full duration-200 hover:bg-[#b7a08a] shadow-[3px_5px_0px_#4f4f4f]  active:shadow-[0px_0px_0px_#4f4f4f] active:duration-100  active:translate-y-[2px] active:translate-x-[2px] "
         >
           <Image src={Coffee} alt="Darkmode" className="  h-10 w-8" />
