2a1b97c3e05c36c4ac953d6a957bdcab5086c66f
Marquis Sampson <marquis4484@gmail.com>
2025-07-10 16:10:36 -0400

    comic title update

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import React from "react";
 import { useState } from "react";
-
 import Image from "next/image";
 import Link from "next/link";
 import Darkmode from "../public/images/buttons/Subtract.png";
@@ -10,18 +9,22 @@ import LinkedIn from "../public/images/buttons/linkedIn_button.png";
 import Coffee from "../public/images/buttons/BMAC_button.png";
 
 function Home() {
-
-
   return (
     <div id="frontpage" className="bg-bannerImg">
-      <div id="elements_frontpage" >
-        <div id="button_position" className="h-[140px] w-[185px] flex items-end justify-end" >
-          <button id="darkmode_button"className="bg-[#E3E3E3] p-3 inset-shadow-xs rounded-full duration-200  hover:bg-[#cecece] active:bg-[#9a9a9a] shadow-[3px_5px_0px_#4f4f4f]  active:shadow-[0px_0px_0px_#4f4f4f] active:duration-100  active:translate-y-[2px] active:translate-x-[2px]">
-            <Image src={Darkmode} alt="Darkmode" className="h-10 w-10"/>
+      <div id="elementsOn_frontpage">
+        <div
+          id="button_position"
+          className="h-[120px] w-[165px] flex items-end justify-end"
+        >
+          <button
+            id="darkmode_button"
+            className="bg-[#E3E3E3] p-3 inset-shadow-xs rounded-full duration-200  hover:bg-[#cecece] active:bg-[#9a9a9a] shadow-[3px_5px_0px_#4f4f4f]  active:shadow-[0px_0px_0px_#4f4f4f] active:duration-100  active:translate-y-[2px] active:translate-x-[2px]"
+          >
+            <Image src={Darkmode} alt="Darkmode" className="h-10 w-10" />
           </button>
         </div>
-        <div className="flex flex-col justify-center items-center h-[80vh]">
-          <div className="w-[352px] flex-grow items-stretch bg-primary rounded-[20px] border-4 border-black drop-shadow-lg text-center mb-12 ">
+        <div className="flex flex-col justify-center items-center h-[800px]">
+          <div className="w-[352px] h-[500px] flex items-center justify-center bg-primary rounded-[20px] border-4 border-black drop-shadow-lg text-center mb-12 shadow-[5px_10px_8px_#4f4f4f]  ">
             Comic Title
           </div>
           <Link href="/comic">
@@ -31,10 +34,7 @@ function Home() {
       </div>
 
       <div id="social_links" className="flex justify-evenly">
-        <button
-          
-          className="bg-[#D5BBA1] py-3 px-4 rounded-full duration-200 hover:bg-[#b7a08a] shadow-[3px_5px_0px_#4f4f4f]  active:shadow-[0px_0px_0px_#4f4f4f] active:duration-100  active:translate-y-[2px] active:translate-x-[2px] "
-        >
+        <button className="bg-[#D5BBA1] py-3 px-4 rounded-full duration-200 hover:bg-[#b7a08a] shadow-[3px_5px_0px_#4f4f4f]  active:shadow-[0px_0px_0px_#4f4f4f] active:duration-100  active:translate-y-[2px] active:translate-x-[2px] ">
           <Image src={Coffee} alt="Darkmode" className="  h-10 w-8" />
           {/* #a38f7b */}
         </button>
