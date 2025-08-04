43920b1a1ad37421deb5faa2a58c0c41a10043b8
Marquis Sampson <marquis4484@gmail.com>
2025-08-04 14:34:01 -0400

    title removed

--- app/comic/page.tsx
+++ app/comic/page.tsx
@@ -1,7 +1,4 @@
 import React from "react";
-import React from 'react';
-
-
 import Image from "next/image";
 import Link from "next/link";
 import Darkmode from "../../public/images/buttons/moon_button.png";
@@ -23,16 +20,12 @@ function ComicPreview() {
       ></div>
 
       <div className="flex flex-col items-center">
-        <div className="w-[332px] h-[65px] flex items-center justify-center bg-primary rounded-[20px] border-4 border-black  mb-8 shadow-[5px_10px_8px_#4f4f4f]">
-          Comic Title
-        </div>
         <Link href="/comic">
           <Image src={Comic} alt="Comic" className="w-[17rem] mb-4" />
         </Link>
       </div>
-      
     </div>
-     /*comic page end*/
+    /*comic page end*/
   );
 }
 
