dee37f326223c5404d40c17f90bf28534af0f135
Marquis Sampson <marquis4484@gmail.com>
2025-09-27 12:37:57 -0400

    import

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 //      import React, { useState, useEffect } from "react";
 //     import Image from "next/image";
 //   import Link from "next/link";
-//  import { useTheme } from "next-themes";
+//   import { useTheme } from "next-themes";
 // i mport Comic1 from "../public/images/comic_covers/comic1.png";
 // import Comic2 from "../public/images/comic_covers/comic2.png";
 // import Comic3 from "../public/images/comic_covers/ComingSoon.png";
