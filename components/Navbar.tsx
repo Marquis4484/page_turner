a969d9ed009ee89381effb27897df7aa2fb9898d
Marquis Sampson <marquis4484@gmail.com>
2025-10-24 15:32:27 -0400

    removed key description

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -19,14 +19,14 @@ const Navbar: React.FC<Props> = ({ hidden }) => {
       }`}
     >
       <div className="flex justify-between items-center px-[16px] py-[12px] max-w-5xl mx-auto">
-        <Link href="/" aria-label="Go to home">
+        <Link href="/" aria-label="Go to home" className="">
           <button className="bg-[#D5BBA1] py-[8px] px-[8px] rounded-full duration-200 hover:bg-[#b7a08a] shadow-[3px_5px_0px_#4f4f4f] active:shadow-[0px_0px_0px_#4f4f4f] active:duration-100 active:translate-y-[2px] active:translate-x-[2px]">
             <Image src={home} alt="" className="h-[40px] w-[40px]" />
           </button>
         </Link>
 
         <div className="flex flex-col items-center gap-1 max-[700px]:w-[350px]">
-          <h1 className="text-xl px-10 font-semibold max-[700px]:text-[15px]">
+          <h1 className="text-xl px-10 font-semibold max-[700px]:hidden">
             You can use the arrow keys to scroll through this comic.
           </h1>
           {/* <button className="bg-beige px-4 py-1 rounded-lg text-black text-sm shadow-sm hover:shadow-md transition">
