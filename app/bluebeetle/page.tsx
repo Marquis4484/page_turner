cba69e93b6bb91df0f416c5eb4a5bed9bb7c729a
Marquis Sampson <marquis4484@gmail.com>
2026-03-16 11:23:01 -0400

    Navbar

--- app/bluebeetle/page.tsx
+++ app/bluebeetle/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useEffect, useMemo, useRef, useState } from "react"; 
 import Image from "next/image";
-import Navbar from "@/components/Navbar"; 
+import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";   
 import SettingsPanel from "@/components/SettingsPanel"; 
 
