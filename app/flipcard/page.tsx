d949443c55fb5f0aacff196d4473f95ab47ecb83
Marquis Sampson <marquis4484@gmail.com>
2026-04-26 13:16:07 -0400

    width adjusted

--- app/flipcard/page.tsx
+++ app/flipcard/page.tsx
@@ -2,13 +2,13 @@
 import FlipCard from "@/components/FlipCard";
 
 export default function Home() {
-  
+
   return (
     <main className="min-h-screen grid place-items-center p-8 bg-neutral-100">
       <FlipCard
         frontSrc="/cards/front.jpg"
         backSrc="/cards/back.jpg"
-        width={360} 
+        width={380} 
         height={220}
         roundedClass="rounded-2xl"
         className="hover:scale-[1.02] transition-transform duration-200"
