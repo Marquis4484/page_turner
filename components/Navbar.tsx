15a29d9d4db4f70d275601281e5d206ff6707d8e
Marquis Sampson <marquis4484@gmail.com>
2026-05-31 22:01:26 -0400

    Image Link

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -24,10 +24,10 @@ const Navbar: React.FC<Props> = ({ hidden }) => {
       <div className="flex justify-between items-center px-[16px] py-[12px] max-w-5xl mx-auto">
         <Link href="/"  className="max-[700px]:items-center" aria-label="Go to home" >
           <button className="bg-[#D5BBA1] py-[8px] px-[8px] rounded-full duration-200 hover:bg-[#b7a08a] shadow-[3px_5px_0px_#4f4f4f] active:shadow-[0px_0px_0px_#4f4f4f] active:duration-100 active:translate-y-[2px] active:translate-x-[2px]"> 
-            <Image src={home} alt="" className="h-[40px] w-[40px]" />
+            <Image src={home} alt="" className="h-[40px] w-[40px]"  />
           </button>
         </Link>
-        
+
 
         <div className="flex flex-col items-center gap-1 max-[700px]:w-[350px]">
           <h1 className="text-xl px-10 font-semibold max-[700px]:hidden"> 
