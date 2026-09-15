a03c172eaf2b98ea765229eb37d1518fe35032a6
Marquis Sampson <marquis4484@gmail.com>
2026-09-15 15:55:37 -0400

    reforming image

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next"; 
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
- 
-
 import { ThemeProvider }  from "next-themes";
 import { Analytics } from "@vercel/analytics/next";
 
